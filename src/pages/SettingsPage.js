02f58f0bdb61245a8b16acdeaa5c51ae9251bb15
agastyalabs <pcst.ecrocks@gmail.com>
2025-05-29 21:37:29 +0530

    Update SettingsPage.js

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,8 +1,13 @@
-// src/pages/SettingsPage.js
-import React from &#39;react&#39;;
-import { Moon, Sun, Gift, UserCog, HelpCircle, LogOut } from &#39;lucide-react&#39;;
-import { LoadingSpinner } from &#39;../components/LoadingSpinner&#39;; // Assuming LoadingSpinner.js is in src/components/
-import { formatDateTime } from &#39;../utils/helpers&#39;; // Assuming helpers.js is in src/utils/
+import React from 'react';
 
-const SettingsPage = ({ userId, userProfile, theme, toggleTheme, handleSignOut, navigateToView }) =\> {
-if (\!userProfile) return &lt;LoadingSpinner text=&quot;Loading settings...&quot; /&gt;;
+const SettingsPage = ({ userId, userProfile, db, setError, setSuccess, currentAppId, navigateToView }) => {
+  // Add your SettingsPage page logic and UI here
+  return (
+    <div>
+      <h1>Settings</h1>
+      {/* Page content goes here */}
+    </div>
+  );
+};
+
+export default SettingsPage;
