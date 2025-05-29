6013ec3b2d793a772c75c901ece35ec73b6524bf
agastyalabs <pcst.ecrocks@gmail.com>
2025-05-29 18:44:02 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to SalesOps Pro App</h1>
+      <p>Your React app is working!</p>
+    </div>
+  );
+}
+
+export default App;
