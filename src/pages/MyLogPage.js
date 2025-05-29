83083c2c8697f78f3131d43dddcb5bbc55444d04
agastyalabs <pcst.ecrocks@gmail.com>
2025-05-29 21:36:31 +0530

    Update MyLogPage.js

--- src/pages/MyLogPage.js
+++ src/pages/MyLogPage.js
@@ -1,20 +1,13 @@
-// src/pages/MyLogPage.js
-import React, { useState, useEffect, useCallback } from &#39;react&#39;;
-import { collection, query, onSnapshot, addDoc, serverTimestamp, orderBy, limit } from &#39;firebase/firestore&#39;;
-import { MapPin, Clock, PlusCircle, Sparkles, Home } from &#39;lucide-react&#39;;
-import { Modal } from &#39;../components/Modal&#39;;
-import { InputField } from &#39;../components/InputField&#39;;
-import { LoadingSpinner } from &#39;../components/LoadingSpinner&#39;;
-import { AlertMessage } from &#39;../components/AlertMessage&#39;; // For summaryError
-import { db, currentAppId, auth } from &#39;../utils/firebase&#39;; // Import auth for userId
-import { formatDateTime, fetchAddressFromCoordinates } from &#39;../utils/helpers&#39;;
-import { GEMINI_API_KEY } from &#39;../config&#39;;
-import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from &quot;@google/generative-ai&quot;;
+import React from 'react';
 
-const MyLogPage = ({ userId, userProfile, setError, setSuccess }) =\> { // Removed unused currentAppId from props as it's imported
-const [attendanceLog, setAttendanceLog] = useState([]);
-const [checkIns, setCheckIns] = useState([]);
-const [isLoading, setIsLoading] = useState(true);
-const [isCheckInModalOpen, setIsCheckInModalOpen] = useState(false);
-const [checkInNotes, setCheckInNotes] = useState('');
-const [isCheckingIn, setIsCheckingIn] = useState(false);
+const MyLogPage = ({ userId, userProfile, db, setError, setSuccess, currentAppId, navigateToView }) => {
+  // Add your MyLogPage page logic and UI here
+  return (
+    <div>
+      <h1>My Activity Log</h1>
+      {/* Page content goes here */}
+    </div>
+  );
+};
+
+export default MyLogPage;
