7167a0fa0762eb3d886f37052cac8979c16f6944
agastyalabs <pcst.ecrocks@gmail.com>
2025-05-29 17:50:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'; // <--- Make sure this line is exactly './App'
+import App from '../src/App'; // Try explicitly going up one level then into src
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
